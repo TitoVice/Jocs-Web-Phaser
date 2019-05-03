a941802cf7efee95247fe60315789193cf350282
CatalaHD <thecatalahd@gmail.com>
2019-05-03 21:03:25 +0200

    casella issue correct3ed

--- game/scenes/PlayScene.js
+++ game/scenes/PlayScene.js
@@ -15,6 +15,7 @@ export default class PlayScene extends Scene {
 
   create () {
     console.log("Starting PlayScene ...");
+    console.log('Pantalla: ' + window.innerWidth + ' x ' + window.innerHeight);
     
     let fonsimg = this.add.image(window.innerWidth/2, window.innerHeight/2, 'fons');
     fonsimg.displayWidth=window.innerWidth*2;
@@ -27,29 +28,31 @@ export default class PlayScene extends Scene {
     this.taulerimgY = taulerimg.displayHeight;
 
     const casellamidaX = taulerimg.displayWidth/9;
-    const casellamidaY = 112.2*taulerimg.displayHeight/1008;
+    const casellamidaY = 108*taulerimg.displayHeight/1008;
 
-    let casellaX = 3; let casellaY = 3; let offsetX = 6; let offsetY = 8;
-    let posicioACasellaX = 0; let posicioACasellaY = 0;
+    for (let j=0; j<9; j++) {
+      for (let i=0; i<9; i++) {
+        let casellaX = j; let casellaY = i;
+        let offsetX = 6; let offsetY = 25;
+        let posicioACasellaX = 0; let posicioACasellaY = 0;
 
-    let tmp_posX = window.innerWidth/2 - 0.5 * taulerimg.displayHeight;
-    if (casellaY%2 != 0) {
-      tmp_posX+= casellamidaX*0.5;
+        let tmp_posX = window.innerWidth/2 - 0.5 * taulerimg.displayHeight;
+        if (casellaY%2 != 0) {
+          tmp_posX += casellamidaX*0.5;
+        }
+        let tmp_posY = casellamidaY;
+        
+        posicioACasellaX = (casellaX)*casellamidaX + tmp_posX + offsetX;
+        posicioACasellaY = (casellaY)*casellamidaY + tmp_posY - casellamidaX/2 + offsetY;
+
+        let iconSettings = this.add.image(posicioACasellaX, posicioACasellaY, 'iconSettings').setInteractive();
+        iconSettings.setDisplaySize(casellamidaX,casellamidaX);
+        let that = this;
+        iconSettings.on('pointerup', function(event) {
+          that.scene.start('MenuScene'); // Start the main game.
+        });
+      }
     }
-    let tmp_posY = 112.2*taulerimg.displayHeight/1008;
-
-    posicioACasellaX = (casellaX)*casellamidaX + tmp_posX;
-    posicioACasellaY = (casellaY)*casellamidaY + tmp_posY;
-
-    console.log(posicioACasellaX + ', ' + posicioACasellaY);
-
-    let iconSettings = this.add.image(posicioACasellaX + offsetX, posicioACasellaY - casellamidaX/2 + offsetY, 'iconSettings').setInteractive();
-    iconSettings.setDisplaySize(casellamidaX,casellamidaX);
-    let that = this;
-    iconSettings.on('pointerup', function(event) {
-      that.scene.start('MenuScene'); // Start the main game.
-    }); 
-
     this.posicioText = this.add.text(16, 16, 'Posicio: 0, 0', { fontSize: '32px', fill: '#000'});
     this.posicioX = 0;
     this.posicioY = 0;
@@ -59,7 +62,6 @@ export default class PlayScene extends Scene {
   update () {
     this.posicioX = this.input.mousePointer.x;
     this.posicioY = this.input.mousePointer.y;
-    this.posicioText.setText('Mouse: ' + this.posicioX + ', ' + this.posicioY + ' | Pantalla: ' + window.innerWidth + ' x ' + window.innerHeight +
-    '\nTauler: '+ this.taulerimgX + ' ' + this.taulerimgX);
+    this.posicioText.setText('Mouse: ' + this.posicioX + ', ' + this.posicioY);
   }
 }
